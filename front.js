0e1293084c908c0e76cf6f181f125c6200af3ce2
pseudoc <atlas.yu@canonical.com>
2023-03-18 23:30:00 +0800

    nowrap impl

--- front.js
+++ front.js
@@ -1,1 +1,28 @@
-console.log('Knowledge sharing!');
+const style = document.createElement('style');
+style.type = 'text/css';
+style.textContent = `
+p {
+  width: max-content;
+  max-width: max-content;
+}
+div.comment-text {
+  overflow-x: auto;
+}`;
+
+let wrap = false;
+
+function nowrap() {
+  document.head.append(style);
+  wrap = true;
+}
+
+function undo_nowrap() {
+  document.head.removeChild(style);
+  wrap = false;
+}
+
+function toggle_wrap() {
+  (wrap ? undo_nowrap : nowrap)();
+}
+
+nowrap();
