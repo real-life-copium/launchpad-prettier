aadf69790b2322e6dda26a338805a7da6e9d2386
pseudoc <atlas.yu@canonical.com>
2023-03-19 21:50:50 +0800

    set enabled state

--- back.js
+++ back.js
@@ -6,4 +6,27 @@ function toggle_nowrap(tab) {
   chrome.tabs.sendMessage(tab.id, payload);
 }
 
+// a shorthand
+const dc = chrome.declarativeContent;
+const rules = [{
+  conditions: [
+    new dc.PageStateMatcher({
+      pageUrl: {
+        schemes: ['https'],
+        hostSuffix: 'bugs.launchpad.net',
+        pathContains: '/+bug/',
+      }
+    })
+  ],
+  actions: [ new dc.ShowAction() ]
+}];
+
+function initialize() {
+  chrome.action.disable();
+  dc.onPageChanged.removeRules(undefined, function () {
+    dc.onPageChanged.addRules(rules);
+  });
+}
+
 chrome.action.onClicked.addListener(toggle_nowrap);
+chrome.runtime.onInstalled.addListener(initialize);
